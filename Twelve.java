614e87864a27f4ce7442e5b64ce0214a761112cc
yw_c <2553755963@qq.com>
2020-05-31 09:40:39 +0800

    测试上传

---
+++ Twelve.java
@@ -0,0 +1,25 @@
+package PPT;
+
+public class Twelve implements Runnable{
+	private static int idcnt=1;
+	private final int threadid=idcnt++;
+	int counter=3;
+	public void run(){
+		while(counter>=0){
+			try{ 
+				Thread.sleep(1000); 
+			} catch (Exception e){ e.printStackTrace(); }
+			System.out.println("#"+threadid+
+				(counter>0?"->"+counter:"->run!"));
+			counter--;
+		}
+	}
+	public static void main(String[] args){
+		Thread t1=new Thread(new Twelve());
+		Thread t2=new Thread(new Twelve());
+		t1.start();
+		t2.start();
+		System.out.println("waiting for run...");
+	}
+
+}
